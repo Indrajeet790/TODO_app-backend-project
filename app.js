bd3ccc62d5e314edfee78ae47132ac99e8d3b97b
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-06-03 15:31:39 +0530

    require database connection filr into app.js

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = 4400;
 
+// Require dbConnection
+const { connectToMongoDb } = require("./config/dbConnection");
+// database connection
+connectToMongoDb("mongodb://127.0.0.1:27017/TodoAPP_NODEJS")
+  .then(() => {
+    console.log("database is connected");
+  })
+  .catch((err) => {
+    console.log("database is not connected");
+  });
+
 app.listen(PORT, (err) => {
   if (err) {
     console.log("error");
